679a144358de2186e39395eec085d09e7009ead0
igorpustylnik <darthveider36@gmail.com>
2024-07-03 10:33:51 +0300

    Initial commit

---
+++ Doctor Appointment/TabBar/TabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarController.swift
+//  Doctor Appointment
+//
+//  Created by Игорь Пустыльник on 03.07.2024.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .systemBlue
+    }
+
+}
