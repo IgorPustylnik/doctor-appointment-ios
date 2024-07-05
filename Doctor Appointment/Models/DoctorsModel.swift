8fd55e69d5213d6e000dfca50068978aec9988ea
igorpustylnik <darthveider36@gmail.com>
2024-07-05 12:14:51 +0300

    Add DoctorsModel and edit HomeModel

---
+++ Doctor Appointment/Models/DoctorsModel.swift
@@ -0,0 +1,33 @@
+//
+//  DoctorsModel.swift
+//  Doctor Appointment
+//
+//  Created by Игорь Пустыльник on 05.07.2024.
+//
+
+import Foundation
+
+struct Doctor {
+    let id: Int
+    let name: String
+    let rank: String
+    let photo: String
+}
+
+class DoctorsModel {
+    private var doctors: [Doctor] = [
+        Doctor(id: 0, name: "Imran Syahir", rank: "General Doctor", photo: "imran_syahir"),
+        Doctor(id: 1, name: "Joseph Brostito", rank: "Dental Specialist", photo: "joseph_brostito"),
+        Doctor(id: 2, name: "Bessie Coleman", rank: "Dental Specialist", photo: "bessie_coleman"),
+        Doctor(id: 3, name: "Babe Didrikson", rank: "Dental Specialist", photo: "babe_didrikson"),
+    ]
+    
+    func getDoctor(id: Int) -> Doctor? {
+        for doc in doctors {
+            if doc.id == id {
+                return doc
+            }
+        }
+        return nil
+    }
+}
--- Doctor Appointment/Models/HomeModel.swift
+++ Doctor Appointment/Models/HomeModel.swift
@@ -8,8 +8,7 @@
 import Foundation
 
 struct NextAppointment {
-    let name: String
-    let rank: String
+    let doctor: Doctor
     let dateFrom: Date?
     let dateTill: Date?
 }
@@ -21,8 +20,7 @@ struct CircularButton {
 }
 
 struct NearDoctor {
-    let name: String
-    let rank: String
+    let doctor: Doctor
     let distance: Double
     let rating: Double
     let ratingCount: Int
@@ -33,8 +31,10 @@ class HomeModel {
     let userName: String = "James"
     let profilePicture: String = "profilePicture"
     
-    let nextAppointment = NextAppointment(
-        name: "Dr. Imran Syahir", rank: "General Doctor",
+    private let doctorsModel = DoctorsModel()
+    
+    lazy var nextAppointment = NextAppointment(
+        doctor: doctorsModel.getDoctor(id: 0)!,
         dateFrom: createDate(year: 2024, month: 6, day: 12, hour: 11, minute: 00),
         dateTill: createDate(year: 2024, month: 7, day: 12, hour: 12, minute: 00))
     
@@ -45,15 +45,18 @@ class HomeModel {
         CircularButton(index: 3, name: "Hospital", image: "hospital"),
     ]
     
-    let nearDoctors: [NearDoctor] = [
-        NearDoctor(name: "Dr. Joseph Brostito", rank: "Dental Specialist",
+    lazy var nearDoctors: [NearDoctor] = [
+        NearDoctor(doctor: doctorsModel.getDoctor(id: 1)!,
                    distance: 1.2, rating: 4.8, ratingCount: 120,
-                   openTime: createDate(year: 2024, month: 7, day: 12, hour: 17, minute: 00))
+                   openTime: createDate(year: 2024, month: 7, day: 12, hour: 17, minute: 00)),
+        NearDoctor(doctor: doctorsModel.getDoctor(id: 2)!,
+                   distance: 0.8, rating: 4.6, ratingCount: 74,
+                   openTime: createDate(year: 2024, month: 7, day: 12, hour: 10, minute: 00))
     ]
 }
 
 extension HomeModel {
-    private static func createDate(year: Int, month: Int, day: Int, hour: Int, minute: Int) -> Date? {
+    private func createDate(year: Int, month: Int, day: Int, hour: Int, minute: Int) -> Date? {
         if !(1...12).contains(month) { return nil }
         if !(1...31).contains(day) { return nil }
         if !(0...23).contains(hour) { return nil }
